35cd2d926006c08a3f0be659739e7a4a016c5b8a
nakroth09 <95112460+nakroth09@users.noreply.github.com>
2021-11-29 21:29:34 +0900

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,5 +1,5 @@
 let handler = function (m) {
-  // this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
+  // this.sendContact(m.chat, '6281343989930', 'Rafi', m)
   let contacts = []
   for (let a of Object.keys(Owner)) {
 let owner = Owner[a]
